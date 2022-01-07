1b0aabf2d7a68dc32cc34f5aa4b990cdefb6eb6e
RahulMahotra <54627917+RahulMahotra@users.noreply.github.com>
2022-01-07 14:09:12 +0530

    Create Q8.cpp

---
+++ Amazon/Q8.cpp
@@ -0,0 +1,17 @@
+long long countWays(int m)
+    {
+       long long dp[m+1];
+       
+       dp[0] = 1;
+       dp[1] = 1;
+       
+       for(int i=2; i<=m; i++){
+           
+          dp[i] = dp[i-2] + 1; 
+           
+           
+       }
+       
+       return dp[m];
+       
+    }
