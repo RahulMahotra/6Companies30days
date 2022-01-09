e78ea8f519e62ff6fa80978f3e8961520ccd72ac
RahulMahotra <54627917+RahulMahotra@users.noreply.github.com>
2022-01-09 22:55:29 +0530

    Update Q5.cpp

--- Amazon/Q5.cpp
+++ Amazon/Q5.cpp
@@ -1,16 +1,21 @@
 vector<vector<string>> displayContacts(int n, string contact[], string s)
 {
         int len = s.length();
-        vector<vector<string>> ans; 
+        vector<vector<string>> ans;
     
         for(int i=1; i<=len; i++){
             
             string str = s.substr(0,i);
             vector<string> v;
+            unordered_set<string> s;
             for(int j=0; j<n; j++){
                 
-                if(str == contact[j].substr(0,i) )
+                if(str == contact[j].substr(0,i)){
+                    if(s.find(contact[j]) == s.end())
                     v.push_back(contact[j]); 
+                }
+                s.insert(contact[j]);
+                
             }
             sort(v.begin(), v.end());
             if(v.size() == 0) {
