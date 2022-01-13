699b452c726e4d9833a19c6c7ccc8199b728a4ef
RahulMahotra <54627917+RahulMahotra@users.noreply.github.com>
2022-01-13 19:21:10 +0530

    Create Q2.cpp

---
+++ Microsoft/Q2.cpp
@@ -0,0 +1,43 @@
+vector<int> adj[N];
+	    vector<int> degree0(N, 0);
+	    
+	    for(auto edge: prerequisites)
+	    {
+	        int u = edge.second;
+	        int v = edge.first;
+	        
+	        adj[u].push_back(v);
+	        
+	        degree0[v]++;
+	    }
+	    
+	    queue<int> q;
+	    for(int i = 0; i < N; i++)
+	    {
+	        if(degree0[i] == 0)
+	        {
+	            q.push(i);
+	        }
+	    }
+	    
+	    int count = 0;
+	    
+	    while(!q.empty())
+	    {
+	        int u = q.front();
+	        q.pop();
+	        
+	        for(auto v: adj[u])
+	        {
+	            degree0[v]--;
+	            
+	            if(degree0[v] == 0)
+	            {
+	                q.push(v);
+	            }
+	        }
+	        
+	        count++;
+	    }
+	    
+	    return count == N;
