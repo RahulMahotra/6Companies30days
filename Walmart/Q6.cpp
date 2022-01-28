fad8ca630dcc99fde87479ba1542dbb95cefe969
RahulMahotra <54627917+RahulMahotra@users.noreply.github.com>
2022-01-28 18:46:49 +0530

    Update Q6.cpp

--- Walmart/Q6.cpp
+++ Walmart/Q6.cpp
@@ -1,13 +1,13 @@
-long long power(int N,int R){
+    const long long md = 1e9 + 7; 
+    long long power(int N,int R){
         
        if(R == 0) return 1;
     
-       long long ans = power(N, R/2)%1000000007;
+       long long ans = power(N, R/2) % md;
        
-       ans=(ans*ans)%1000000007;
+       ans = (ans*ans) % md;
        
-       if(R%2==0) return ans%1000000007;
-       
-       return (ans*N)%1000000007;
+       if (R%2 == 0) return ans;
+       else return (ans*N) % md;
         
     }
